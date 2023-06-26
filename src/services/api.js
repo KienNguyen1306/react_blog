8acca6fba3e4abe3d8d74c31da408298d64cb77c
kiennguyen <kienndk09@gmail.com>
2023-06-26 18:38:33 +0700

    pust api

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const API = {
   call: function () {
     return axios.create({
-      baseURL: "http://wp-api.test/wp-json",
+      baseURL: "https://bthxuan.azdigi.shop/",
     });
   },
   callWithToken: function () {
     return axios.create({
-      baseURL: "http://wp-api.test/wp-json",
+      baseURL: "https://bthxuan.azdigi.shop/",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`,
       },
