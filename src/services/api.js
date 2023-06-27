ea7f450f1cb1df09373c42f326804d1da9d58ec1
kiennguyen <kienndk09@gmail.com>
2023-06-27 08:06:27 +0700

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const API = {
   call: function () {
     return axios.create({
-      baseURL: "https://bthxuan.azdigi.shop/",
+      baseURL: "https://bthxuan.azdigi.shop/wp-json",
     });
   },
   callWithToken: function () {
     return axios.create({
-      baseURL: "https://bthxuan.azdigi.shop/",
+      baseURL: "https://bthxuan.azdigi.shop/wp-json",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`,
       },
